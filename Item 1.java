d08d4e7cd63d8283174d7b66e77e5f14824fc5ac
Michelle D. Almen <117343266+learningmich@users.noreply.github.com>
2023-01-08 11:51:33 +0800

    Update Item 1.java

--- Item 1.java
+++ Item 1.java
@@ -1,3 +1,20 @@
-public class Item 1
+public class Item_1 extends Lazada
 {
+  String Item_info;
+	public Item_1(String product_name, int price, String Item_info) {
+		super(product_name, price);
+		this.Item_info=Item_info;
+	}
+
+
+	public String item_info() {
+		return "First Item.";
+	}
+
+	public String getItemInfo() {
+		return Item_info;
+	}
+	public void setItemInfo(String Item_info) {
+		this.Item_info=Item_info;
+	}
 }
