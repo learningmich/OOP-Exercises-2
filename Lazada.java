568a015f35336b72608567c7180f63823aa7df14
Michelle D. Almen <117343266+learningmich@users.noreply.github.com>
2023-01-08 11:49:26 +0800

    Update Lazada.java

--- Lazada.java
+++ Lazada.java
@@ -1,3 +1,26 @@
-public class Lazada
+public  abstract class Lazada
 {
+  protected String product_name;
+	protected int price;
+
+	public Lazada(String product_name, int price) {
+		this.product_name=product_name;
+		this.price=price;
+	}
+
+	public abstract String item_info();
+
+	public String getProductName(){
+		return product_name;
+	}
+	public void setProductName(String product_name) {
+		this.product_name=product_name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price=price;
+	}
 }
